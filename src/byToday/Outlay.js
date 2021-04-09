972f409134a6c37024a8ca84910578617bdbc365
dkeka0127 <sori_127@naver.com>
2021-04-09 18:51:23 +0900

    TodayAdd Page Income, Outlay 변환

---
+++ src/byToday/Outlay.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+function OutLay() {
+    return (
+        <View>
+            <Text>지출 Page</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'blue',
+    }
+});
+
+export default OutLay;
